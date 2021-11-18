5b3dfc8897d01022730be650bdc04cea906752e8
Rocky <sourabhchoudharysml810gmail.com>
2021-11-18 19:50:42 +0530

    long first commit

---
+++ client/build/precache-manifest.5461a80dd17be3aab20d09d1fdca97ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1403321c57741addfd63b1662025c9ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f2ec6fcf77f3f5a6e0",
+    "url": "/static/css/main.1e4856ca.chunk.css"
+  },
+  {
+    "revision": "3f55c3df7f5be652eb9b",
+    "url": "/static/js/2.d54d52a9.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.d54d52a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f2ec6fcf77f3f5a6e0",
+    "url": "/static/js/main.6ec424c3.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
